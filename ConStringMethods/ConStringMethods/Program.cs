3e6a6dd62c912ec14ed193c12fa1f962c7bb76a8
Amisha Patidar <amishapatidar98@gmail.com>
2021-01-27 21:13:53 +0530

    Example of String and StringBuilder methods

---
+++ ConStringMethods/ConStringMethods/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConStringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str, str1;
+            int choice;
+            Console.WriteLine("Enter your Choice\n1 for Compare strings\n2 for Concat Strings\n3 for Split string\n4 for check equality\n5 for Replace\n6 for exit");
+            do
+            {
+                Console.WriteLine("Enter Your Choice");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter string 1 to Compare");
+                        str = Console.ReadLine();
+                        Console.WriteLine("Enter string 2 to Compare");
+                        str1 = Console.ReadLine();
+                        Console.WriteLine(string.Compare(str, str1));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter string 1 to Concat");
+                        str = Console.ReadLine();
+                        Console.WriteLine("Enter string 2 to Concat");
+                        str1 = Console.ReadLine();
+                        Console.WriteLine(string.Concat(str, str1));
+                        //str = str + str1;
+                        //Console.WriteLine(str);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter String to split");
+
+                        str = Console.ReadLine();
+                        string[] s2 = str.Split(' ');
+                        foreach (string str2 in s2)
+                        {
+                            Console.WriteLine(str2);
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter String 1 to check equality");
+                        str = Console.ReadLine();
+                        Console.WriteLine("Enter String 2 to check equality");
+                        str1 = Console.ReadLine();
+                        Console.WriteLine(str.Equals(str1));
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter String to Replace");
+                        str = "Hello C# welcome";
+                        Console.WriteLine(str.Replace("C#", "DotNet"));
+                        break;
+
+                    default:
+                        Console.WriteLine("wrong choice");
+                        break;
+
+                }
+            } while (choice > 0 && choice<=5);
+        }
+    }
+}
---
+++ ConStringMethods/ConStringMethods/StringBuilderClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConStringMethods
+{
+    class StringBuilderClass
+    {
+        static void Main()
+        {
+            StringBuilder str = new StringBuilder();
+            int option;
+            Console.WriteLine("Enter your Choice\n1 for Append\n2 for AppendLine\n3 for Replace\n4 for Length\n5 for Capacity\n6 for exit");
+            do
+            {
+                Console.WriteLine("Enter your choice to execute");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter String to Append");
+                        string address = Console.ReadLine();
+                        Console.WriteLine(str.Append(address)); //Append at the end of current string
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter String 1 to Append in new Line");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter string 2 to Append ");
+                        string surname = Console.ReadLine();
+                        Console.WriteLine("Appended strings are:");
+                        Console.WriteLine(str.AppendLine(name)); //Append string in new line
+                        Console.WriteLine(str.AppendLine(surname)); //Append string in new line
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter String");
+                        string str1 = Console.ReadLine();
+                        Console.WriteLine("Enter string to replace ");
+                        string oldStr = Console.ReadLine();
+                        Console.WriteLine("Enter string to replace the previous entered string ");
+                        string newStr = Console.ReadLine();
+                        Console.WriteLine($"New String is {str1.Replace(oldStr, newStr)}");
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter String to check Length");
+                        string addressStr = Console.ReadLine();
+                        Console.WriteLine(addressStr.Length);
+                        break;
+                    case 5:
+                        Console.WriteLine(str.Capacity);
+                        string strr = Console.ReadLine();
+                        str.Append(strr);
+                        Console.WriteLine(str.Capacity);
+                        break;
+                    default: Console.WriteLine("wrong choice"); break;
+                }
+            } while (option>0 && option<=5);
+        }
+    }
+}
